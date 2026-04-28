ce8fa5c27bdbdbd66ad02f9e0522262484127335
Raphaël FROMENTIN <rafbodaha@gmail.com>
2026-04-28 16:42:39 +0200

    feat: Basic DB & Argon2id setup

--- main.cpp
+++ main.cpp
@@ -1,19 +1,57 @@
+#include <sodium.h>
 #include <QCoreApplication>
 #include <QSqlDatabase>
 #include <QSqlQuery>
 #include <QSqlError>
+
+int hashPassword(QString password, char hash[crypto_pwhash_STRBYTES]) {
+    return crypto_pwhash_str(
+        hash,
+        password.toUtf8().constData(),
+        password.toUtf8().size(),
+        crypto_pwhash_OPSLIMIT_INTERACTIVE,
+        crypto_pwhash_MEMLIMIT_INTERACTIVE
+    );
+}
+
+bool verifyPassword(QString password, QString hash) {
+    return crypto_pwhash_str_verify(hash.toUtf8().constData(), password.toUtf8().constData(), password.toUtf8().size()) == 0;
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     qInfo() << "Hello World";
 
+    if (sodium_init() < 0) {
+        return 1;
+    }
+
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
     db.setDatabaseName("database.sqlite");
 
     if (!db.open()) {
         qDebug() << "Error:" << db.lastError().text();
+        return 1;
     }
+
+    QSqlQuery query;
+    query.prepare("SELECT id, name, password FROM users WHERE name = :name");
+    query.bindValue(":name", "admin");
+
+    if(!query.exec() || !query.next()) {
+        qDebug() << "User not found or query failed";
+        return 1;
+    }
+
+    int id = query.value(0).toInt();
+    QString name = query.value(1).toString();
+    QString hash = query.value(2).toString().toUtf8().constData();
+
+    qDebug() << id << name << hash;
+
+    QString password = "admin";
+    qDebug() << "Check:" << verifyPassword(password, hash);
 
     return a.exec();
 }
--- setup.sql
+++ setup.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name VARCHAR(100) NOT NULL,
+    password VARCHAR(97) NOT NULL,
     admin BOOLEAN NOT NULL
 );
 
@@ -11,4 +12,15 @@ CREATE TABLE IF NOT EXISTS messages (
     content VARCHAR(2000) NOT NULL,
     FOREIGN KEY (sender) REFERENCES users(id),
     FOREIGN KEY (receiver) REFERENCES users(id)
+);
+
+-- 
+-- Argon2id
+-- User: admin
+-- Password: admin
+INSERT OR IGNORE INTO users (id, name, password, admin) VALUES (
+  0,
+  'admin',
+  '$argon2id$v=19$m=65536,t=2,p=1$Ib6cOs8SiSrqrh1yT73PoQ$SKtzHnh16339ZdooY2VB/BqihcxH9D2CNgHIJaYO2ew',
+  1
 );
