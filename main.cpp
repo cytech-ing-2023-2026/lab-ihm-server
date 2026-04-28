eab4b734f7ff1c05da4fc04bf9840b2b4c8ea471
Raphaël FROMENTIN <rafbodaha@gmail.com>
2026-04-28 15:45:15 +0200

    feat: Add DB support

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <QCoreApplication>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     qInfo() << "Hello World";
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("database.sqlite");
+
+    if (!db.open()) {
+        qDebug() << "Error:" << db.lastError().text();
+    }
+
     return a.exec();
 }
